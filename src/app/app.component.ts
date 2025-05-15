edd4ab7a856a2218057ef370fed09aff75cf274d
rflmoreira <rafaelmoreira.lima@hotmail.com>
2025-05-14 23:56:59 -0300

    Tela de carregamento adicionada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,39 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoadingComponent } from './loading/loading.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ModalContatoComponent } from './modal-contato/modal-contato.component';
+import { HomeComponent } from './home/home.component';
 import { SobreMimComponent } from './sobre-mim/sobre-mim.component';
+import { HabilidadesComponent } from './habilidades/habilidades.component';
 import { ProjetosComponent } from './projetos/projetos.component';
 import { ModalProjetoComponent } from './modal-projeto/modal-projeto.component';
-import { ModalContatoComponent } from './modal-contato/modal-contato.component';
-import { HabilidadesComponent } from './habilidades/habilidades.component';
-import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
   imports: [
+    CommonModule,
+    LoadingComponent,
     NavbarComponent,
+    ModalContatoComponent,
+    HomeComponent,
     SobreMimComponent,
+    HabilidadesComponent,
     ProjetosComponent,
     ModalProjetoComponent,
-    ModalContatoComponent,
-    HabilidadesComponent,
-    HomeComponent,
     FooterComponent
-  ]
+  ],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'portfolio'; // Substitua pelo título da sua aplicação
+  loading = true;
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.loading = false;
+    }, 2000);
+  }
 }
